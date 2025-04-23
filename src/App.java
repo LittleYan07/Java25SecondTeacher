99041ad7ada7100468c8502e115dc54d6a45c69b
Yan <0415yanlee@gmail.com>
2025-04-23 11:34:53 +0800

    TestDev

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 123456789!");
 
-        System.out.println("Hello, World! I am Edward No. 956846!");
+        System.out.println("Hello, World! I am Edward No. 9!");
     }
 }
