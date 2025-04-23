2137c3f19288066255d1945cb2d2b2c6a3e36108
Yan <0415yanlee@gmail.com>
2025-04-23 13:35:22 +0800

    Update on github #3

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 12345!");
+        System.out.println("Hello, World!");
 
-        System.out.println("Hello, World! I am Edward No. 67890!");
+        System.out.println("Hello, World!");
     }
 }
